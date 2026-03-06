377ca61ca6805c942b0bd92743f052d5bc7284d7
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-03-07 01:24:52 +0600

    Added login page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
-console.log("Connecting!!!!!");
+// console.log("Connecting!!!!!");
+
+document.getElementById("login-btn").addEventListener('click', () => {
+
+    const inputUserName = document.getElementById('input-user-name');
+    const userName = inputUserName.value;
+    console.log(userName);
+
+    const inputPassword = document.getElementById('input-password');
+    const password= inputPassword.value;
+    console.log(password);
+
+    if (userName === "admin" && password === "admin123") {
+        alert("login success");
+    }
+    else {
+        alert("login failed");
+    }
+
+})
